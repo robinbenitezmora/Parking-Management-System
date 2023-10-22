0961532f5cf230cec8d89f4b5ec083d6847d3826
robinbenitezmora <robinbenitez1990@gmail.com>
2023-10-22 12:01:38 -0500

    Refactor #1 'FormMenu.java' file

--- Form/FormMenu.java
+++ Form/FormMenu.java
@@ -1,6 +1,68 @@
-private void btnLoginActionPerformed(java.awt.event.ActionEvent evt) {                                         
- FormMenu formMenu = new FormMenu();
- formMenu.setVisible(true);
- this.dispose();
+import java.awt.Color;
+
+import javax.swing.JButton;
+
+public class FormMenu extends javax.swing.JFrame {
+ public FormMenu() {
+  initComponents();
+  setLocationRelativeTo(null);
+  this.getContentPane().setBackground(Color.WHITE);
+ }
+
+ @SuppressWarnings("unchecked")
+ // <editor-fold defaultstate="collapsed" desc="Generated Code">
+
+ private void initComponents() {
+
+  JButton btnTambah = new javax.swing.JButton();
+  JButton btnLihat = new javax.swing.JButton();
+  JButton btnKeluar = new javax.swing.JButton();
+
+  setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+  setTitle("Menu Utama");
+
+  btnTambah.setText("Tambah Data");
+  btnTambah.addActionListener(new java.awt.event.ActionListener() {
+   public void actionPerformed(java.awt.event.ActionEvent evt) {
+    actionPerformed(evt);
+   }
+  });
+
+  btnLihat.setText("Lihat Data");
+  btnLihat.addActionListener(new java.awt.event.ActionListener() {
+   public void actionPerformed(java.awt.event.ActionEvent evt) {
+    actionPerformed(evt);
+   }
+  });
+
+  btnKeluar.setText("Keluar");
+  btnKeluar.addActionListener(new java.awt.event.ActionListener() {
+   public void actionPerformed(java.awt.event.ActionEvent evt) {
+    actionPerformed(evt);
+   }
+  });
+
+  javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+  getContentPane().setLayout(layout);
+  layout.setHorizontalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+    .addGroup(layout.createSequentialGroup().addGap(30, 30, 30)
+      .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+        .addComponent(btnKeluar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE,
+          Short.MAX_VALUE)
+        .addComponent(btnLihat, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE,
+          Short.MAX_VALUE)
+        .addComponent(btnTambah, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE,
+          Short.MAX_VALUE))
+      .addContainerGap(30, Short.MAX_VALUE)));
+  layout.setVerticalGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+    .addGroup(layout.createSequentialGroup().addGap(30, 30, 30)
+      .addComponent(btnTambah, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+      .addGap(18, 18, 18)
+      .addComponent(btnLihat, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+      .addGap(18, 18, 18)
+      .addComponent(btnKeluar, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+      .addContainerGap(30, Short.MAX_VALUE)));
+
+  pack();
+ }// </editor-fold>
 }
-```
